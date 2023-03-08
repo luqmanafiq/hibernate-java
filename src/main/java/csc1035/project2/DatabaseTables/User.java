11c80758b99721af3eea9d328d865cd9fddaa74c
oliverbravery <107476394+oliverbravery@users.noreply.github.com>
2023-03-08 13:21:27 +0000

    Moved database User static manipulation methods to User class (previously in DatabaseIO)

--- src/main/java/csc1035/project2/DatabaseTables/User.java
+++ src/main/java/csc1035/project2/DatabaseTables/User.java
@@ -1,9 +1,12 @@
 package csc1035.project2.DatabaseTables;
 
+import csc1035.project2.DatabaseIO;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "tblUser")
@@ -24,4 +27,56 @@ public class User {
     }
 
     public void setUsername(String username) { this.username = username; }
+
+    /**
+     * Gets user related to the given username
+     * @param username : String of the username of the user (case-insensitive)
+     * @return User : User object representing the user. Null if not user associated with the username
+     * **/
+    public static User GetUser(String username) {
+        List<User> userResponse = (List<User>)(Object)DatabaseIO.HQLQueryDatabase(String.format("FROM User WHERE " +
+                "Username = '%s'", username.toLowerCase()));
+        if(userResponse.size() == 0) {return null;}
+        return userResponse.get(0);
+    }
+
+    /**
+     * Checks if a user exists based on a given username (String)
+     * @param username : String (case-insensitive)
+     * @return Boolean: True if user exists, false if not
+     * **/
+    public static Boolean CheckUserExists(String username) {
+        User u = GetUser(username);
+        if (u == null) {return false;}
+        return true;
+    }
+
+    /**
+     * Checks whether a username (case-insensitive) is unique (valid) then adds the user to the table
+     * (username will be lowercase added to the database).
+     * Returns TblUser object of the new user if successful or null if the name was invalid.
+     * @param username : String representation of the username
+     * @return : TblUser object representing all user table information
+     * **/
+    public static User AddUser(String username) {
+        if(!CheckUserExists(username)) {
+            User usr = new User(username.toLowerCase());
+            DatabaseIO.AddToDatabase(usr);
+            return usr;
+        }
+        return null;
+    }
+
+    /**
+     * Removes a given user from the tblUser by the username of the user (case-insensitive).
+     * @param username : String representing the username entity attribute
+     * @return int : 1 if user does not exist, 0 if user existed and was removed successfully
+     * **/
+    public static int RemoveUser(String username) {
+        if (!CheckUserExists(username)) {
+            return 1;
+        }
+        DatabaseIO.RemoveFromDatabase(GetUser(username));
+        return 0;
+    }
 }
